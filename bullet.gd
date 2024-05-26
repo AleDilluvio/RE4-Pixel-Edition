33ee8c9d75dde2b53f8535bfc488559d91c15e7b
Alejo Dilluvio <dilluvioalejo@gmail.com>
2024-05-26 13:01:33 -0300

    cambios en bullet

--- bullet.gd
+++ bullet.gd
@@ -1,10 +1,20 @@
 extends Area2D
 
-@export var speed = 2000
+@export var speed = 1500
 
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	position += (Vector2.RIGHT*speed).rotated(rotation) * delta # que hace esto?
+	position += (Vector2.RIGHT*speed) * delta # no estoy seguro de q esto funcione
 	
+func _physics_process(delta):
+	await(get_tree().create_timer(0.01))
+	set_physics_process(false)
+
+
+func _on_visible_on_screen_enabler_2d_screen_exited():
+	queue_free() # La bala desaparece si abandona la pantalla.
+
+func _on_body_entered(_body):
+	queue_free() # La bala desaparece si le pega a alguien.
--- player.gd
+++ player.gd
@@ -2,11 +2,12 @@ extends Area2D
 
 @export var speed = 400 # Velocidad del jugador (pixels/seg).
 var screen_size # Tamaño pantalla.
+var can_fire = true
+var bullet = preload("res://bullet.tscn")
 
 # Se llama cuando carga por primera vez.
 func _ready():
 	screen_size = get_viewport_rect().size
-
 
 # Se llama en cada frame, delta es la velocidad de los frames.
 func _process(delta):
@@ -32,3 +33,14 @@ func _process(delta):
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_h = velocity.x < 0 # Voltea la imagen si la vel es < 0.
+		
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("fire") and can_fire:
+		var bullet_instance = bullet.instantiate()
+		#bullet_instance.global_position = lerp(position.x, get_parent().position.x, 0.5)
+		#Ver como poder obtener la ubicacion del arma, y el lado a donde apunta.
+		get_parent().add_child(bullet_instance)
+		can_fire = false
+		await(get_tree().create_timer(0.2))
+		can_fire = true
