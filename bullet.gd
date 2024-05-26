97bf6a57169020034261f68e378244d6ca1c0904
Alejo Dilluvio <dilluvioalejo@gmail.com>
2024-05-26 14:18:41 -0300

    agregada funcionalidad en disparo y background

--- bullet.gd
+++ bullet.gd
@@ -2,16 +2,28 @@ extends Area2D
 
 @export var speed = 1500
 
+func _set_speed(value):
+	speed = value
+
+func _get_speed():
+	return speed
+	
+func _get_position(property):
+	return position
+
+func _set_position(x,y):
+	position.x = x
+	position.y = y
+
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	position += (Vector2.RIGHT*speed) * delta # no estoy seguro de q esto funcione
-	
+	position += (Vector2.RIGHT*speed) * delta # No estoy seguro de q esto funcione.
+
 func _physics_process(delta):
 	await(get_tree().create_timer(0.01))
 	set_physics_process(false)
-
 
 func _on_visible_on_screen_enabler_2d_screen_exited():
 	queue_free() # La bala desaparece si abandona la pantalla.
--- player.gd
+++ player.gd
@@ -1,9 +1,13 @@
 extends Area2D
 
 @export var speed = 400 # Velocidad del jugador (pixels/seg).
+var direction = 1 # Seteo en uno para que empiece disparando para la derecha.
 var screen_size # Tamaño pantalla.
 var can_fire = true
 var bullet = preload("res://bullet.tscn")
+
+func _get_position():
+	return position
 
 # Se llama cuando carga por primera vez.
 func _ready():
@@ -14,8 +18,10 @@ func _process(delta):
 	var velocity = Vector2.ZERO # Vector de jugador.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
+		direction = 1 # Disparo a la derecha.
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
+		direction = 0 # Disparo a la izquierda.
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
@@ -33,13 +39,20 @@ func _process(delta):
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_h = velocity.x < 0 # Voltea la imagen si la vel es < 0.
-		
+	
+	#direction = Input.get_axis("ui_left", "ui_right") No me funcionó. Probar de nuevo.
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("fire") and can_fire:
 		var bullet_instance = bullet.instantiate()
-		#bullet_instance.global_position = lerp(position.x, get_parent().position.x, 0.5)
-		#Ver como poder obtener la ubicacion del arma, y el lado a donde apunta.
+		if direction:
+			bullet_instance._set_speed(abs(bullet_instance._get_speed()))
+			bullet_instance._set_position(_get_position().x + 64, _get_position().y - 10) # Ver como poder obtener la ubicacion del arma, para no hardcodear.
+		else:
+			bullet_instance._set_speed(abs(bullet_instance._get_speed())*(-1))
+			bullet_instance.get_child(0).flip_h = true
+			bullet_instance._set_position(_get_position().x - 64,_get_position().y - 10) # Ver como poder obtener la ubicacion del arma, para no hardcodear.
+		
 		get_parent().add_child(bullet_instance)
 		can_fire = false
 		await(get_tree().create_timer(0.2))
