6d8c38619a313364a6a475feb7f0e78aa1b6eeaf
Alejo Dilluvio <dilluvioalejo@gmail.com>
2024-07-08 19:21:38 -0300

    agregada funcion de recibir daño en marie

--- marie.gd
+++ marie.gd
@@ -13,10 +13,49 @@ func _ready():
 func _process(delta):
 	if life_points <= 0:
 		# Falta animacion de muerte.
-		#await(get_tree().create_timer(0.01))
-		queue_free()
+		#get_tree().create_timer(0.01).timeout
+		#queue_free()
+		pass
+	
+	_physics_process(delta)
+	
+	if direction == 1:
+		$AnimatedSprite2D.flip_h = false
+	elif direction == 0:
+		$AnimatedSprite2D.flip_h = true
+	
+	#$AnimatedSprite2D.flip_h = true # Ver como hacer para que mire hacia el personaje.
+	var velocity = Vector2.ZERO
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
 
 func _on_area_entered(area):
 	if area.is_in_group("Ammo"): # Corregir.
 		life_points -= 1
-		position.x -= -10
+		# Falta animacion de recibir daño.
+		position.x += 2.5 # Falta animacion de recibir daño.
+		position.y -= 5
+		await get_tree().create_timer(0.02).timeout
+		position.x += 2.5
+		position.y -= 5
+		await get_tree().create_timer(0.05).timeout
+		position.y += 5
+		position.x += 2.5
+		await get_tree().create_timer(0.02).timeout
+		position.y += 5
+		position.x += 2.5
+		
+func _physics_process(delta):
+	if $Ray_Damage_Left.is_colliding() and $Ray_Damage_Left.get_collider(0).is_in_group("Ammo"):
+		direction = 0
+		
+	if $Ray_Damage_Right.is_colliding() and $Ray_Damage_Right.get_collider(0).is_in_group("Ammo"):
+		direction = 1
+		position.x -= 5 # Falta animacion de recibir daño.
+		position.y -= 10
+		await get_tree().create_timer(0.1).timeout
+		position.y += 10
+		position.x -= 5
